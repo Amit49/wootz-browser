33bcb5ccde3ab91b028edcb6478e04ab8c7a2c46
1311-hack1 <sagarexternaluse@gmail.com>
2024-10-26 08:27:42 +0200

    Tab switcher crash fix #147

--- src/chrome/browser/hub/internal/android/java/src/org/chromium/chrome/browser/hub/HubManagerImpl.java
+++ src/chrome/browser/hub/internal/android/java/src/org/chromium/chrome/browser/hub/HubManagerImpl.java
@@ -139,6 +139,9 @@ public class HubManagerImpl implements HubManager, HubController {
 
     @Override
     public @Nullable View getPaneHostView() {
+        if (mHubCoordinator == null) {
+            ensureHubCoordinatorIsInitialized();
+        }
         assert mHubCoordinator != null : "Access of a Hub pane host view that doesn't exist";
         return mHubContainerView.findViewById(R.id.hub_pane_host);
     }
