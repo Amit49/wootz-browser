42e9e73b8c172dbb40b6a748d379be194b5354ae
DevT75 <dekuta75@gmail.com>
2024-11-11 13:21:32 +0100

    code cleanup

--- src/chrome/browser/extensions/chrome_content_browser_client_extensions_part.cc
+++ src/chrome/browser/extensions/chrome_content_browser_client_extensions_part.cc
@@ -796,10 +796,12 @@ void ChromeContentBrowserClientExtensionsPart::OverrideWebkitPrefs(
 
 void ChromeContentBrowserClientExtensionsPart::BrowserURLHandlerCreated(
     BrowserURLHandler* handler) {
-  // handler->AddHandlerPair(&ExtensionWebUI::HandleChromeURLOverride,
-  //                         BrowserURLHandler::null_handler());
-  // handler->AddHandlerPair(BrowserURLHandler::null_handler(),
-  //                         &ExtensionWebUI::HandleChromeURLOverrideReverse);
+#if 0
+  handler->AddHandlerPair(&ExtensionWebUI::HandleChromeURLOverride,
+                          BrowserURLHandler::null_handler());
+  handler->AddHandlerPair(BrowserURLHandler::null_handler(),
+                          &ExtensionWebUI::HandleChromeURLOverrideReverse);
+#endif
 }
 
 void ChromeContentBrowserClientExtensionsPart::
